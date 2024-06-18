3e16fd3e2dec47a7599e54cd05aff21513c7b09a
Andy <hinchliffe106@gmail.com>
2024-06-18 16:45:09 +0100

    description

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "LoFiRadio",
-  description: "Discover the best lofi beats to relax and study to with our comprehensive guide. Lofi music has become the go-to choice for those seeking a calm and focused atmosphere, perfect for studying, working, or simply unwinding. In this guide, we'll introduce you to the top YouTube channels, Spotify playlists, and artists that deliver the most soothing and effective lofi beats. Whether you're looking for 24/7 live streams or curated playlists, we've got you covered.",
+  description: "Discover the best lofi beats for studying, working, and relaxing. Explore top YouTube channels, Spotify playlists, and artists in our comprehensive guide.",
 };
 
 export default function RootLayout({ children }) {
